bb81fb9b04b9baae31b9e1a16eeb9b77ae4bdd1a
innocentk33 <innocentk33@gmail.com>
2022-03-29 18:54:58 +0000

    splash change

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cemedo/constants/constants.dart';
 import 'package:cemedo/constants/theme.dart';
 import 'package:cemedo/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
@@ -21,7 +22,7 @@ class SplashScreen extends StatelessWidget {
 
                 Text("Bienvenue sur", style: TextStyle(
                   color: Colors.white,
-                  fontWeight: FontWeight.w600
+                  fontWeight: FontWeight.w600, fontFamily: kAppFontFamily
                 ),),
                 Assets.images.lOGOCEMEDOPng.image(),
                 GestureDetector(
@@ -29,11 +30,9 @@ class SplashScreen extends StatelessWidget {
 
                   },
                   child: Container(
-                    padding: EdgeInsets.only(
-                        top: 8,
-                        bottom: 8,
-                        left: 10,
-                        right: 10
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8,
+                        horizontal: 10
                     ),
                     color: kOrange.withAlpha(80),
                     child: Text("Continuer", style: TextStyle(
